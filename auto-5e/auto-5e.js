0be5c9fdbaf3ee7804db3562f48d1d5e5a0250b0
Reaver01 <jawmac@gmail.com>
2019-10-16 21:02:08 -0600

    Initial commit

---
+++ auto-5e/auto-5e.js
@@ -0,0 +1,126 @@
+// @author Reaver#4634
+
+var addExperience;
+
+Hooks.on('preDeleteCombat', (entity, options) => {
+    DistributeExperience(entity);
+});
+
+Hooks.on('updateToken', (entity, options) => {
+    SyncTokenStatus(entity);
+});
+
+Hooks.on('updateCombat', entity => {
+    PanOnNextTurn(entity);
+});
+
+// Distributes Experience to any living PCs in the combat tracker after ending combat if
+// addExperience Toggle has been set true. It adds together any experience from all HOSTILE NPC tokens
+// in the tracker, divides it by the number of PCs and adds it to their character sheet.
+function DistributeExperience(entity) {
+    defeatedEnemies = entity.turns.filter(object => (!object.actor.isPC && object.defeated && object.token.disposition === -1));
+    players = entity.turns.filter(object => (object.actor.isPC && !object.defeated));
+    experience = 0;
+    if (defeatedEnemies.length > 0 && addExperience) {
+        defeatedEnemies.forEach(enemy => {
+            experience += enemy.actor.data.data.details.xp.value;
+        });
+        if (players.length > 0) {
+            dividedExperience = Math.floor(experience / players.length);
+            experienceMessage = '<b>Experience Awarded!</b><p><b>' + dividedExperience + '</b> added to:</br>';
+            players.forEach(player => {
+                const actor = game.actors.entities.find(actor => actor.id === player.actor.data._id);
+                actor.update({
+                    'data.details.xp.value': player.actor.data.data.details.xp.value + dividedExperience
+                });
+                experienceMessage += player.actor.data.name + '</br>';
+            });
+            experienceMessage += '</p>';
+            ChatMessage.create({
+                user: game.user._id,
+                speaker: {
+                    actor: this.actor
+                },
+                content: experienceMessage
+            });
+        }
+    }
+}
+
+// Syncs Token HP and deceased status in the combat tracker. Goes well with Combat Utility Belt marking
+// Bloodied and dead: https://raw.githubusercontent.com/death-save/combat-utility-belt/beta/module.json
+function SyncTokenStatus(entity) {
+    if (!entity.scene.active) {
+        return;
+    }
+    const combat = game.combat;
+    if (combat) {
+        combatant = combat.data.combatants.find(token => token.tokenId == entity.data.id);
+        tokenHp = entity.actor.data.data.attributes.hp.value;
+        if (combatant) {
+            combat.updateCombatant({
+                id: combatant.id,
+                defeated: (tokenHp == 0)
+            });
+        }
+    }
+}
+
+// Pans players to the token who's turn it is in combat. Also selects the token if they own it.
+// This will eventually get configs so panning can be turned off for players.
+function PanOnNextTurn(entity) {
+    if (!game.combat || game.combat.turns.length === 0) {
+        return;
+    }
+    token = entity.turns[entity.current.turn].token;
+    let xCoord = token.x;
+    let yCoord = token.y;
+    canvas.animatePan({
+        x: xCoord,
+        y: yCoord
+    });
+    const canvasToken = canvas.tokens.get(token.id);
+    canvasToken.control();
+}
+
+class Auto5e {
+    // Eventually this will be replaced for combatibility.
+    // This function replaces Foundry's Native endCombat.
+    async endCombat() {
+        return new Promise((resolve, reject) => {
+            if (!game.user.isGM) {
+                reject('You cannot end an active combat');
+            }
+            new Dialog({
+                title: `End Combat?`,
+                content: '<p>End this combat encounter and empty the turn tracker?</p>',
+                buttons: {
+                    yes: {
+                        icon: '<i class="fas fa-check"></i>',
+                        label: 'End Combat',
+                        callback: () => {
+                            addExperience = false;
+                            this.delete().then(resolve);
+                        }
+                    },
+                    xp: {
+                        icon: '<i class="fas fa-check"></i>',
+                        label: 'End with XP',
+                        callback: () => {
+                            addExperience = true;
+                            this.delete().then(resolve);
+                        }
+                    },
+                    no: {
+                        icon: '<i class="fas fa-times"></i>',
+                        label: 'Cancel'
+                    }
+                },
+                default: 'yes'
+            }).render(true);
+        });
+    }
+}
+
+// Replace Default endCombat with Auto5e's
+Combat.prototype.endCombat = Auto5e.prototype.endCombat;
